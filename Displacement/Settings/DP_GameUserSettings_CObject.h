1d68921e07ebaca08d2d4389fe0b687aa395c91e
SinkingMeteor <badcode@internet.ru>
2024-02-26 13:14:10 +0200

    Game user settings cpp class

---
+++ Displacement/Settings/DP_GameUserSettings_CObject.h
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "DP_GameUserSettings_CObject.generated.h"
+
+UCLASS(Blueprintable)
+class DISPLACEMENT_API UDP_GameUserSettings_CObject : public UGameUserSettings
+{
+	GENERATED_BODY()
+};
