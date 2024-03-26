a16f6f40480d20e6e36f0259dd030fa6dd0a8a33
SinkingMeteor <badcode@internet.ru>
2024-03-26 11:30:57 +0200

    Effect volume system hotfix

--- Displacement/Gameplay/EffectVolume/DP_EffectVolumeSystem_ACC.cpp
+++ Displacement/Gameplay/EffectVolume/DP_EffectVolumeSystem_ACC.cpp
@@ -4,6 +4,7 @@
 #include "DP_EffectVolumeSystem_ACC.h"
 
 #include "EventSystem/U5_EventSystem_ACC.h"
+#include "EventSystem/U5_Event_BlueprintLibrary.h"
 #include "Utils/U5_Utils.h"
 
 UDP_EffectVolumeSystem_ACC::UDP_EffectVolumeSystem_ACC()
@@ -16,7 +17,7 @@ void UDP_EffectVolumeSystem_ACC::RegisterEffectVolume(ADP_EffectVolume_CA* _effe
 	if(!_effectVolume) return;
 	
 	FString volumeHandleString{};
-	UU5_EventSystem_ACC::EventHandleToString(volumeHandleString, _effectVolume->VolumeHandle);
+	UU5_Event_BlueprintLibrary::EventHandleToString(volumeHandleString, _effectVolume->VolumeHandle);
 
 	_effectVolume->OnVolumeChangedEvent().AddUObject(this, &ThisClass::UpdateVolumeReceivers);
 
@@ -52,7 +53,7 @@ void UDP_EffectVolumeSystem_ACC::UnregisterEffectVolume(ADP_EffectVolume_CA* _ef
 void UDP_EffectVolumeSystem_ACC::RegisterEffectVolumeReceiver(UDP_EffectVolumeReceiver_ACC* _effectVolumeReceiver)
 {
 	FString volumeHandleString{};
-	UU5_EventSystem_ACC::EventHandleToString(volumeHandleString, _effectVolumeReceiver->TargetVolumeHandle);
+	UU5_Event_BlueprintLibrary::EventHandleToString(volumeHandleString, _effectVolumeReceiver->TargetVolumeHandle);
 	
 	FDP_EffectVolumeData* foundData = VolumeReceivers.Find(volumeHandleString);
 
@@ -73,7 +74,7 @@ void UDP_EffectVolumeSystem_ACC::RegisterEffectVolumeReceiver(UDP_EffectVolumeRe
 void UDP_EffectVolumeSystem_ACC::UnregisterEffectVolumeReceiver(UDP_EffectVolumeReceiver_ACC* _effectVolumeReceiver)
 {
 	FString volumeHandleString{};
-	UU5_EventSystem_ACC::EventHandleToString(volumeHandleString, _effectVolumeReceiver->TargetVolumeHandle);
+	UU5_Event_BlueprintLibrary::EventHandleToString(volumeHandleString, _effectVolumeReceiver->TargetVolumeHandle);
 	FDP_EffectVolumeData* foundData = VolumeReceivers.Find(volumeHandleString);
 
 
@@ -85,7 +86,7 @@ void UDP_EffectVolumeSystem_ACC::UnregisterEffectVolumeReceiver(UDP_EffectVolume
 void UDP_EffectVolumeSystem_ACC::UpdateVolumeReceivers(ADP_EffectVolume_CA* _effectVolume)
 {
 	FString volumeHandleString{};
-	UU5_EventSystem_ACC::EventHandleToString(volumeHandleString, _effectVolume->VolumeHandle);
+	UU5_Event_BlueprintLibrary::EventHandleToString(volumeHandleString, _effectVolume->VolumeHandle);
 	
 	FDP_EffectVolumeData* foundData = VolumeReceivers.Find(volumeHandleString);
 
@@ -102,7 +103,6 @@ void UDP_EffectVolumeSystem_ACC::UpdateVolumeReceivers(ADP_EffectVolume_CA* _eff
 
 void UDP_EffectVolumeSystem_ACC::UpdateVolumeReceiver(UDP_EffectVolumeReceiver_ACC* _effectVolumeReceiver, const FVector& _volumeLocation, const FVector& _volumeBounds)
 {
-	mU5_DEBUGOUT(true, mDNAME(_effectVolumeReceiver) << " " << _volumeLocation.ToString() << " " << _volumeBounds.ToString());
 	_effectVolumeReceiver->UpdateEffectAppearance(_volumeLocation, _volumeBounds);
 }
 
